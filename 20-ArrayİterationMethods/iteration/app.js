92bc21c23725470f1190b138f3e8a439563278e0
sebine2704 <161808827+sebine2704@users.noreply.github.com>
2024-04-09 06:44:24 +0400

    Add files via upload

---
+++ 20-ArrayİterationMethods/iteration/app.js
@@ -0,0 +1,429 @@
+
+//Tapsiriq
+//1- 2 ədədin cəmini tapan funskiya yazın. Ədədlər parametr olaraq verilməlidir.
+// let sumnumbs =(firstnum,secondnum)=>{
+//     console.log(firstnum+secondnum)
+//     }
+//     sumnumbs(35, 40)
+    
+
+//2- 2 ədədin hasili tapan funskiya yazın. Ədədlər parametr olaraq verilməlidir.
+// let sumnumbs =(firstnum,secondnum)=>{
+//    console.log(firstnum*secondnum)
+//   }
+//   sumnumbs(35, 4)
+
+
+//4- Parametr olaraq daxil edilən 2 ədəddən hansının böyük olduğunu return edən funksiya yazın
+// let max =(a,b)=>{
+//   if(a>b){
+//     return a
+//   }
+//   else if(a<b){
+//     return b
+//   }
+// }
+// console.log(max(5,20))
+
+
+//7- Parametr olaraq qəbul edilən arrayın elementləri cəmini hesablayan funksiya yazın.
+// let numarr = [1, 2, 3];
+// let arr2 = numarr.reduce((sum,numbers)=>sum+=numbers)
+// console.log(arr2)
+
+
+
+// let students = [
+//     {
+//     id: 1,
+//     name: "Nurane",
+//     surname: "Ismayilzade",
+//     age: 21,
+//     hobbies: ["music", "walking", "watchingfilm"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "cavid" },
+//     { id: 4, name: "alisa" },
+//     ],
+//     loginDetail: { username: "nurana21", password: "nunu123" },
+//     gender: "girl",
+//     boyfriendGirlfriend: false,
+//     fail: false,
+//     avgPoint: 88,
+//     salaryAZN: 144,
+//     university: "BDU",
+//     },
+//     {
+//     id: 2,
+//     name: "Gizilgul",
+//     surname: "Allahverdiyeva",
+//     age: 20,
+//     hobbies: ["book", "writing code"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "babaxan" },
+//     { id: 4, name: "gulshen" },
+//     ],
+//     loginDetail: { username: "allahverdieva1", password: "salam123" },
+//     gender: "girl",
+//     boyfriendGirlfriend: true,
+//     fail: false,
+//     avgPoint: 70,
+//     salaryAZN: 100,
+//     university: "BDU",
+//     },
+//     {
+//     id: 3,
+//     name: "Xanim",
+//     surname: "Nuriyeva",
+//     age: 21,
+//     hobbies: ["book", "music"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "hikmet" },
+//     { id: 4, name: "gulsen" },
+//     ],
+//     loginDetail: { username: "xanim123", password: "salamBaku" },
+//     gender: "girl",
+//     boyfriendGirlfriend: false,
+//     fail: false,
+//     avgPoint: 80,
+//     salaryAZN: 145,
+//     university: "BDU",
+//     },
+//     {
+//     id: 4,
+//     name: "Minaya",
+//     surname: "Binnetova",
+//     age: 21,
+//     hobbies: ["book", "gaming", "movie", "music"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "anar" },
+//     ],
+//     loginDetail: { username: "binnetova", password: "hello123" },
+//     gender: "girl",
+//     boyfriendGirlfriend: false,
+//     fail: false,
+//     avgPoint: 90,
+//     salaryAZN: 142,
+//     university: "BDU",
+//     },
+//     {
+//     id: 5,
+//     name: "Sabina",
+//     surname: "Hatamli",
+//     age: 21,
+//     hobbies: ["shopping", "listen to music"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "Mirvari" },
+//     ],
+//     loginDetail: { username: "sebine123", password: "salam123" },
+//     gender: "girl",
+//     boyfriendGirlfriend: true,
+//     fail: false,
+//     avgPoint: 81,
+//     salaryAZN: 146,
+//     university: "BDU",
+//     },
+    
+//     {
+//     id: 6,
+//     name: "Ləman",
+//     surname: "Şamilova",
+//     age: 20,
+//     hobbies: [
+//     "watching movies",
+//     "reading books",
+//     "painting",
+//     " walking",
+//     " listen to music",
+//     ],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     ],
+//     loginDetail: { username: "lemanshamilova", password: "salamBaku" },
+//     gender: "girl",
+//     boyfriendGirlfriend: false,
+//     fail: false,
+//     avgPoint: 85.8,
+//     salaryAZN: 145,
+//     university: "BDU",
+//     },
+//     {
+//     id: 7,
+//     name: "Narmin",
+//     surname: "Musayeva",
+//     age: 21,
+//     hobbies: ["book", "gaming", "painting", "walking"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "fidan" },
+//     { id: 4, name: "Irada" },
+//     ],
+//     loginDetail: {
+//     username: "narmin0",
+//     password: "narmin01",
+//     },
+//     gender: "girl",
+//     boyfriendGirlfriend: false,
+//     fail: false,
+//     avgPoint: 80,
+//     salaryAZN: 196,
+//     university: "BDU",
+//     },
+    
+//     {
+//     id: 8,
+//     name: "Fatima",
+//     surname: "Akhundzada",
+//     age: 20,
+//     hobbies: ["drawing", "sleeping"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "Valida" },
+//     ],
+//     loginDetail: { username: "aynur123", password: "salamBaku" },
+//     gender: "girl",
+//     boyfriendGirlfriend: true,
+//     fail: false,
+//     avgPoint: 76,
+//     salaryAZN: 100,
+//     university: "BDU",
+//     },
+//     {
+//     id: 9,
+//     name: "Elmir",
+//     surname: "Huseynov",
+//     age: 21,
+//     hobbies: ["book", "ice skating", "Tennis", "Karting"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "Gurban" },
+//     { id: 2, name: "Hajar" },
+//     { id: 3, name: "Morad" },
+//     { id: 4, name: "Fikrat" },
+//     ],
+//     loginDetail: { username: "huseynovelmirr", password: "maxverstappen" },
+//     gender: "man",
+//     boyfriendGirlfriend: false,
+//     fail: true,
+//     avgPoint: 76,
+//     salaryAZN: 120,
+//     university: "BDU",
+//     },
+//     {
+//     id: 10,
+//     name: "Fidan",
+//     surname: "Rehimli",
+//     age: 21,
+//     hobbies: ["book", "gaming"],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "Turan" },
+//     ],
+//     loginDetail: { username: "fidan123", password: "fidanrahimli" },
+//     gender: "girl",
+//     boyfriendGirlfriend: true,
+//     fail: true,
+//     avgPoint: 75,
+//     salaryAZN: 98,
+//     university: "BDU",
+//     },
+//     {
+//     id: 11,
+//     name: "Aynur",
+//     surname: "Aynurova",
+//     age: 20,
+//     hobbies: ["book", "gaming"],
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     ],
+//     loginDetail: { username: "aynur123", password: "salamBaku" },
+//     gender: "girl",
+//     boyfriendGirlfriend: true,
+//     fail: true,
+//     avgPoint: 81,
+//     salaryAZN: 120,
+//     university: "BDU",
+//     },
+    
+//     {
+//     id: 12,
+//     name: "Elman",
+//     surname: "Muradov",
+//     age: 20,
+//     hobbies: [
+//     "book",
+//     "gaming",
+//     "sking",
+//     "wrestling",
+//     "swiming",
+//     "arguing about philosophy",
+//     "solving sudoku",
+//     "hiking",
+//     "walking",
+//     ],
+//     student: true,
+//     teacher: [
+//     { id: 1, name: "gurban" },
+//     { id: 2, name: "hajar" },
+//     { id: 3, name: "jale" },
+//     { id: 4, name: "akhmed" },
+//     ],
+//     loginDetail: { username: "elman17", password: "elman12345" },
+//     gender: "girl",
+//     boyfriendGirlfriend: false,
+//     fail: true,
+//     avgPoint: 76,
+//     salaryAZN: 100,
+//     university: "BDU",
+//     },
+//     ];
+
+  
+  //task1- Bütün elementləri konsola yazın
+//   students.forEach((element) => {
+//     console.log(element)
+//   }); 
+
+
+
+
+//task2- Bütün tələbələrin adlarını konsola yazın - ad: Qurban
+//  students.forEach((element )=> {
+//     console.log("Ad: ",element.name)
+//  });
+
+
+
+//task3 - Bütün tələbələrin adlarını və soyadlarını konsola yazın - ad: Qurban, soyad: Qurbanzada
+// students.forEach((element)=> {
+//     console.log("Ad: ",element.name,",", "Soyad:",element.surname)
+// });
+
+
+//task4 - Kəsri olan tələbələrin ad və soyadını çapa verin
+// let arr = students.filter((element)=> element.fail==true).map(element=>{
+//     console.log(element.name, element.surname)
+// })
+
+
+
+//task5- - Ən çox hobbisi olan tələbənin ad və hobbilərini çapa verin
+//  let arr = students.filter((element)=> element.hobbies.length>).map(element=>{
+
+//  })
+
+
+
+
+ //task8- boyfriend-i olan tələbələrin adlarını və username-lərini çapa verin
+// let arr = students.filter((element)=> element.boyfriendGirlfriend==true).map(element=>{
+//     console.log(element.name,",", element.loginDetail.username)
+
+// })
+
+
+//task9 - yaşı 20 olan tələbələrin adlarını və müəllim adlarını çapa verin
+// let arr = students.filter((element)=> element.age==20).map(element=>{
+        
+// })
+//task10- Bütün tələbələrə təqaüdlərinin dollar ilə görünməsini təmin edin
+
+// students.forEach((element)=>{
+//     element.salaryUSD = element.salaryAZN*1.7;
+//     console.log(element)
+// })
+
+
+//task14- Tələbələrin adlarını və müəllim saylarını çapa verin
+// students.forEach((element)=>{
+//     console.log("Ad: ",element.name,"," ,"Muellim sayi: ",element.teacher.length)
+
+// })
+
+
+//task15- Tələbələrin adlarını username və şifrələrindən ibarət yeni array yaradın [{name:qurban,username:qurban123,password:qqq123}]
+// let newarr =students.reduce((total, element)=>{
+//     return newarr.push(element.name)
+
+// },[])
+// console.log(newarr)
+
+
+//task20- Bütün tələbələrin yaşlarının toplamı nə qədərdir
+// let sumofage =students.reduce((sum, element)=>{
+//     return sum+element.age
+
+// },0)
+// console.log(sumofage)
+
+
+// task21 - Bütün tələbələrin cəmi bal ortalaması nə qədərdir
+// let box = 0
+// let averageofpoint =students.map((element)=>{
+//     box+=element.avgPoint
+// }) 
+// console.log(box/12)  
+
+
+// task22- "book" həvəsi olan tələbələrin siyahısını çap edin;
+// let arr = students.filter((element)=> element.hobbies.includes("book"))
+// console.log(arr)
+
+
+
+//task24-Hər bir tələbə üçün cinsiyət və yaşı göstərən bir obyekt yaradin
+// let newarr =students.map((element)=>{
+//     let newarr={
+//         name: element.name,
+//         gender: element.gender,
+//         age: element.age
+//     }
+// return newarr
+// })
+// console.log(newarr)
+
+//task25- Bütün tələbələrin ad və soyadlarının uzunluqlarının cəmi hansıdır
+// let sumlength =students.reduce((sum,element)=>sum+element.name.length+element.surname.length,0)
+// console.log(sumlength)
+
+
+//task26- Tələbələrin adlarını tərsinə çevirin.
+
+
+//task27 - "gamig" həvəsi olan tələbələrin array-ini yaradın
+// let arr1= students.filter((element)=> element.hobbies.includes("gaming"))
+// console.log(arr1)
+
+
+
+
+// ----------------------------TASK2-------------------------
+//
+//1- 2 ədədin cəmini tapan funskiya yazın. Ədədlər parametr olaraq verilməlidir.
+// let numarr= [1, 73, 99, 20];
+// console.log(numarr.at(0))
+// console.log(numarr.at(1,2))
+
