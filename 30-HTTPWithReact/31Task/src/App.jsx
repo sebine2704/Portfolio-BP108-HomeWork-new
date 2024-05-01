345e1f796a3c507f962faf75559b7699a8697521
sebine2704 <sebinehetemli5211@gmail.com>
2024-05-01 22:07:06 +0400

    task30

---
+++ 30-HTTPWithReact/31Task/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TableApi from "./components/TableApi"
+
+
+function App() {
+  
+  return (
+    <>
+    <TableApi/>
+    </>
+  )
+}
+
+export default App
